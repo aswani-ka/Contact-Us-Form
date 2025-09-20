7429af54ddb016aca34770dd28977b8cfd2da7b3
ASWANI K A <aswanika05@gmail.com>
2025-09-20 12:11:11 +0530

    feat: add event listener to the form with the empty field check

---
+++ script.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const firstName =  document.getElementById("fname")
+    const firstnamePara =  document.getElementById("fpara")
+    const lastName =  document.getElementById("lname")
+    const lastNamePara =  document.getElementById("lpara")
+    const userEmail =  document.getElementById("email")
+    const userEmailPara =  document.getElementById("epara")
+    const queryPara =  document.getElementById("radioPara")
+    const message =  document.getElementById("msg")
+    const messagePara =  document.getElementById("msgPara")
+    const userConsent =  document.getElementById("consent")
+    const consentPara =  document.getElementById("consentPara")
+    const submitButton =  document.getElementById("submit-btn")
+    const formSubmit =  document.getElementById("form-submit")
+
+    formSubmit.addEventListener("submit", (e) => {
+        e.preventDefault()
+        
+        if(firstName.value.trim() === "") {
+            firstName.classList.add("border-red-500")
+            firstnamePara.classList.remove("hidden")
+        }
+        else {
+            firstName.classList.remove("border-red-500")
+            firstnamePara.classList.add("hidden")
+        }
+        if(lastName.value.trim() === "") {
+            lastName.classList.add("border-red-500")
+            lastNamePara.classList.remove("hidden")
+        }
+        else {
+            lastName.classList.remove("border-red-500")
+            lastNamePara.classList.add("hidden")
+        }
+        if(message.value.trim() === "") {
+            message.classList.add("border-red-500")
+            messagePara.classList.remove("hidden")
+        }
+        else {
+            message.classList.remove("border-red-500")
+            messagePara.classList.add("hidden")
+        }
+    })
+})
